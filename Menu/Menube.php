2e7b0ae088833ef724e9f50a4d71991c140355cd
Mohanadfa <Mohanad-fahad@hotmil.com>
2025-04-12 19:32:46 +0300

    new st

---
+++ Menu/Menube.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="MenuPiT.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>الصفحة الرئيسية</title>
+</head>
+<body>
+
+<footer>
+  <br><br>
+
+  <nav class="sections">
+ 
+  </nav>
+
+  <div class="menu">
+    <div class="menu-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRhgCFrejJwH9Ra_U75JXmf5ef9FZtB0NSLpg&s" alt="وجبة 1">
+      <h3>شاورما</h3>
+      <p></p>
+      <p class="price">57 ر.س</p>
+
+      <div class="rating" id="rating1">
+        <input value="5" name="rating1" id="star1-5" type="radio">
+        <label for="star1-5">★</label>
+        <input value="4" name="rating1" id="star1-4" type="radio">
+        <label for="star1-4">★</label>
+        <input value="3" name="rating1" id="star1-3" type="radio">
+        <label for="star1-3">★</label>
+        <input value="2" name="rating1" id="star1-2" type="radio">
+        <label for="star1-2">★</label>
+        <input value="1" name="rating1" id="star1-1" type="radio">
+        <label for="star1-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://media-cdn.tripadvisor.com/media/photo-p/14/9c/5e/d2/photo4jpg.jpg" alt="وجبة 2">
+      <h3> فيوتشيني الفريدو</h3>
+      <p class="price">89 ر.س</p>
+
+      <div class="rating" id="rating2">
+        <input value="5" name="rating2" id="star2-5" type="radio">
+        <label for="star2-5">★</label>
+        <input value="4" name="rating2" id="star2-4" type="radio">
+        <label for="star2-4">★</label>
+        <input value="3" name="rating2" id="star2-3" type="radio">
+        <label for="star2-3">★</label>
+        <input value="2" name="rating2" id="star2-2" type="radio">
+        <label for="star2-2">★</label>
+        <input value="1" name="rating2" id="star2-1" type="radio">
+        <label for="star2-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://cdn-0.saudiarestaurants.com/wp-content/uploads/2019/10/%D8%A8%D9%8A%D8%B1%D8%AC%D8%B1-%D9%85%D8%B7%D8%B9%D9%85-%D8%A8%D9%8A%D9%84%D9%88.png" alt="وجبة 3">
+      <h3>برجر مشوي</h3>
+      <p class="price">49 ر.س</p>
+
+      <div class="rating" id="rating3">
+        <input value="5" name="rating3" id="star3-5" type="radio">
+        <label for="star3-5">★</label>
+        <input value="4" name="rating3" id="star3-4" type="radio">
+        <label for="star3-4">★</label>
+        <input value="3" name="rating3" id="star3-3" type="radio">
+        <label for="star3-3">★</label>
+        <input value="2" name="rating3" id="star3-2" type="radio">
+        <label for="star3-2">★</label>
+        <input value="1" name="rating3" id="star3-1" type="radio">
+        <label for="star3-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://rest-ksa.arbdar.com/wp-content/uploads/2023/07/%D9%85%D8%B7%D8%B9%D9%85-%D8%A7%D8%A8%D9%86-%D8%A7%D9%84%D9%88%D8%B2%D9%8A%D8%B1-%D8%A7%D9%84%D8%B1%D9%8A%D8%A7%D8%B6.jpg" alt="وجبة 4">
+      <h3>بيتزا خضار</h3>
+      <p class="price">68 ر.س</p>
+
+      <div class="rating" id="rating4">
+        <input value="5" name="rating4" id="star4-5" type="radio">
+        <label for="star4-5">★</label>
+        <input value="4" name="rating4" id="star4-4" type="radio">
+        <label for="star4-4">★</label>
+        <input value="3" name="rating4" id="star4-3" type="radio">
+        <label for="star4-3">★</label>
+        <input value="2" name="rating4" id="star4-2" type="radio">
+        <label for="star4-2">★</label>
+        <input value="1" name="rating4" id="star4-1" type="radio">
+        <label for="star4-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTs4QdA4O6KXHoYccaZQHd2uAU9fBAQh85sfA&s" alt="وجبة 5">
+      <h3>بيتزا بيلو</h3>
+      <p class="price">56 ر.س</p>
+
+      <div class="rating" id="rating5">
+        <input value="5" name="rating5" id="star5-5" type="radio">
+        <label for="star5-5">★</label>
+        <input value="4" name="rating5" id="star5-4" type="radio">
+        <label for="star5-4">★</label>
+        <input value="3" name="rating5" id="star5-3" type="radio">
+        <label for="star5-3">★</label>
+        <input value="2" name="rating5" id="star5-2" type="radio">
+        <label for="star5-2">★</label>
+        <input value="1" name="rating5" id="star5-1" type="radio">
+        <label for="star5-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://cdn-cndob.nitrocdn.com/TNVcUtOAPuqGFiUbahIgLHOviDAJtxsp/assets/images/optimized/rev-4d9dfb3/media.thechefz.co/content/thump_profile/zZ_jEzpED9E0ZUrzsPDyi1dCSD5l5B5O.jpeg" alt="وجبة 6">
+      <h3>سوفليه</h3>
+      <p class="price">68 ر.س</p>
+
+      <div class="rating" id="rating6">
+        <input value="5" name="rating6" id="star6-5" type="radio">
+        <label for="star6-5">★</label>
+        <input value="4" name="rating6" id="star6-4" type="radio">
+        <label for="star6-4">★</label>
+        <input value="3" name="rating6" id="star6-3" type="radio">
+        <label for="star6-3">★</label>
+        <input value="2" name="rating6" id="star6-2" type="radio">
+        <label for="star6-2">★</label>
+        <input value="1" name="rating6" id="star6-1" type="radio">
+        <label for="star6-1">★</label>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</body>
+</html>
+
+
---
+++ Menu/Menuto.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html lang="ar">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="MenuPiT.css"> 
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.1/css/all.min.css" integrity="sha512-DTOQO9RWCH3ppGqcWaEA1BIZOC6xxalwEsw9c2QQeAIftl+Vegovlnee1c9QX4TctnWMn13TZye+giMm8e2LwA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <title>الصفحة الرئيسية</title>
+</head>
+<body>
+
+<footer>
+  <br><br>
+
+  <nav class="sections">
+ 
+  </nav>
+
+  <div class="menu">
+    <div class="menu-item">
+      <img src="https://i0.wp.com/dleel.hayaak.com/wp-content/uploads/2022/09/2022-05-04-1-1.jpg?resize=1024%2C768&ssl=1" alt="وجبة 1">
+      <h3>سوشي بالرز</h3>
+      <p></p>
+      <p class="price">57 ر.س</p>
+
+      <div class="rating" id="rating1">
+        <input value="5" name="rating1" id="star1-5" type="radio">
+        <label for="star1-5">★</label>
+        <input value="4" name="rating1" id="star1-4" type="radio">
+        <label for="star1-4">★</label>
+        <input value="3" name="rating1" id="star1-3" type="radio">
+        <label for="star1-3">★</label>
+        <input value="2" name="rating1" id="star1-2" type="radio">
+        <label for="star1-2">★</label>
+        <input value="1" name="rating1" id="star1-1" type="radio">
+        <label for="star1-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTrMqcD0O_fxhBvASTmgp_pLmRRQEkYZGVXag&s" alt="وجبة 2">
+      <h3>ستيك لحم</h3>
+      <p class="price">49 ر.س</p>
+
+      <div class="rating" id="rating2">
+        <input value="5" name="rating2" id="star2-5" type="radio">
+        <label for="star2-5">★</label>
+        <input value="4" name="rating2" id="star2-4" type="radio">
+        <label for="star2-4">★</label>
+        <input value="3" name="rating2" id="star2-3" type="radio">
+        <label for="star2-3">★</label>
+        <input value="2" name="rating2" id="star2-2" type="radio">
+        <label for="star2-2">★</label>
+        <input value="1" name="rating2" id="star2-1" type="radio">
+        <label for="star2-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://ar.timeoutjeddah.com/cloud/artimeoutjeddah/2023/07/03/ND4hWrAj-tokyo-restaurant-1024x768.jpg" alt="وجبة 3">
+      <h3>أرز مع سلطة الجرجير</h3>
+      <p class="price">87 ر.س</p>
+
+      <div class="rating" id="rating3">
+        <input value="5" name="rating3" id="star3-5" type="radio">
+        <label for="star3-5">★</label>
+        <input value="4" name="rating3" id="star3-4" type="radio">
+        <label for="star3-4">★</label>
+        <input value="3" name="rating3" id="star3-3" type="radio">
+        <label for="star3-3">★</label>
+        <input value="2" name="rating3" id="star3-2" type="radio">
+        <label for="star3-2">★</label>
+        <input value="1" name="rating3" id="star3-1" type="radio">
+        <label for="star3-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://www.timeoutriyadh.com/cloud/timeoutriyadh/2022/05/11/Myazu-1024x768.jpg" alt="وجبة 4">
+      <h3>كنافة</h3>
+      <p class="price">68 ر.س</p>
+
+      <div class="rating" id="rating4">
+        <input value="5" name="rating4" id="star4-5" type="radio">
+        <label for="star4-5">★</label>
+        <input value="4" name="rating4" id="star4-4" type="radio">
+        <label for="star4-4">★</label>
+        <input value="3" name="rating4" id="star4-3" type="radio">
+        <label for="star4-3">★</label>
+        <input value="2" name="rating4" id="star4-2" type="radio">
+        <label for="star4-2">★</label>
+        <input value="1" name="rating4" id="star4-1" type="radio">
+        <label for="star4-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://cdn-0.saudiarestaurants.com/wp-content/uploads/2022/01/%D8%B4%D9%88%D8%B1%D8%A8%D8%A9-%D8%B7%D9%88%D9%83%D9%8A%D9%88.png" alt="وجبة 5">
+      <h3>شوربه يابانيه</h3>
+      <p class="price">120 ر.س</p>
+
+      <div class="rating" id="rating5">
+        <input value="5" name="rating5" id="star5-5" type="radio">
+        <label for="star5-5">★</label>
+        <input value="4" name="rating5" id="star5-4" type="radio">
+        <label for="star5-4">★</label>
+        <input value="3" name="rating5" id="star5-3" type="radio">
+        <label for="star5-3">★</label>
+        <input value="2" name="rating5" id="star5-2" type="radio">
+        <label for="star5-2">★</label>
+        <input value="1" name="rating5" id="star5-1" type="radio">
+        <label for="star5-1">★</label>
+      </div>
+    </div>
+
+    <div class="menu-item">
+      <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTk_FJSoQGMdBErG5bMO31o0ITRR97C1kUKOw&s" alt="وجبة 6">
+      <h3>فيوتشيني</h3>
+      <p class="price">80 ر.س</p>
+
+      <div class="rating" id="rating6">
+        <input value="5" name="rating6" id="star6-5" type="radio">
+        <label for="star6-5">★</label>
+        <input value="4" name="rating6" id="star6-4" type="radio">
+        <label for="star6-4">★</label>
+        <input value="3" name="rating6" id="star6-3" type="radio">
+        <label for="star6-3">★</label>
+        <input value="2" name="rating6" id="star6-2" type="radio">
+        <label for="star6-2">★</label>
+        <input value="1" name="rating6" id="star6-1" type="radio">
+        <label for="star6-1">★</label>
+      </div>
+    </div>
+  </div>
+</footer>
+
+</body>
+</html>
+
+
